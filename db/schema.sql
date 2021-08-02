19daa567b046935ebf58d62766670fb617c00ec3
wijeremy <jeremydavwilliams@gmail.com>
2021-08-02 15:26:04 -0500

    getting started, i just made the schema stuff and a seed file. havent tried running it yet to see if i messed anything up.

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS company_db;
+
+-- CREATE DATABASE
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+)
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT 
+)
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+)
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,37 @@
+INSERT INTO departments (name)
+VALUES 
+    ('Upper Management')
+    ('Sales'),
+    ('HR'),
+    ('IT'),
+    ('Support');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+    ('CEO', 1000000.00, 1),
+    ('Sales Manager', 100000.00, 2),
+    ('Sales Rep', 50000.00, 2),
+    ('HR Lead', 75000.00, 3),
+    ('HR Associate', 50000.00, 3),
+    ('IT Manager', 100000.00, 4),
+    ('IT Associate', 50000.00, 4),
+    ('Support Lead', 50000.00, 5),
+    ('Support Associate', 30000.00, 5);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('John', 'Ownsyou', 1, null),
+    ('Betty', 'Smith', 2, 1),
+    ('Chris', 'Kendal', 4, 1),
+    ('Jeremy', 'Williams', 6, 1),
+    ('Amanda', 'OToole', 8, 1),
+    ('Mike', 'Pluton', 3, 2),
+    ('Shery', 'Waites', 3, 2),
+    ('Connor', 'Connors', 3, 2),
+    ('Deborah', 'Katz', 3, 2),
+    ('Brandon', 'Andrews', 5, 3),
+    ('Mike', 'Punch', 5, 3),
+    ('Pluto', 'The Cat', 7, 4),
+    ('Candie', 'Pierce-Winters', 9, 5),
+    ('Marie Claire', 'Sullivan', 9, 5),
+    ('Tim', 'Acioli', 9, 5);
