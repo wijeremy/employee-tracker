3ebaba875314ea6f4e1e188d53d03623557fe7cc
wijeremy <jeremydavwilliams@gmail.com>
2021-08-02 21:50:00 -0500

    holy mother of god. i had my resolve reject switched around and then proceded to make so many stupid mistakes. all that time for a function that makes a role from inquirer inputs. but IT FUCKING WORKS BABY YIPPY KAY YAY!

--- test.js
+++ test.js
@@ -40,9 +40,9 @@ const viewDepartments = () => {
     });
 }
 
-const getDeparments = () => {
+const getDepartments = () => {
     return new Promise(async (resolve, reject) => {
-        db.query('SELECT id AS "ID", name AS "Deparment" FROM departments;', function (err, results) {
+        db.query('SELECT id AS "ID", name AS "Deparment" FROM departments;', (err, results) => {
             if(err){
                 reject(err)
             }
@@ -56,10 +56,57 @@ const getDeparments = () => {
     })
 }
 
-const tryMe = async () => {
-    console.log('getting promise:')
-    const results = await getDepartments()
-    console.log(`Here you go: ${results}`)
+const isNum = (input) => {
+    if (isNaN(input)) {
+      return 'Please provide a number'
+    }
+    return true
+  }
+
+const promptNewRole = (departments) => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your new role?',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary for this role?',
+        validate: isNum
+      },
+      {
+        type: 'list',
+        name: 'department',
+        message: 'Which department will this role be in?',
+        choices: departments
+      }
+    ])
 }
 
-tryMe()
+
+const getDepartmentId = (department) => {
+    return new Promise ((resolve, reject) => {
+        db.query(`SELECT id FROM departments WHERE name = "${department}";`, (err, results) => {
+            if (err){
+                reject (err)
+            }
+            const key = Object.keys(results[0])[0]
+            resolve(results[0][key])
+        })
+    })
+}
+
+
+const setNewRole = async () => {
+    const departments = await getDepartments();
+    const results = await promptNewRole(departments);
+    const { title, salary, department } = results;
+    const departmentId = await getDepartmentId(department)
+    db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${title}', ${salary}, ${departmentId});`, (err) => {
+        err? console.error(err) : console.log("New role successfully created!")
+    })
+}
+
+setNewRole()
